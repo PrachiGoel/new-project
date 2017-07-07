9ed8a139bf801ad66a9de980c0787f37d5ea8603
prachi <prachi.goel@comparepolicy.com>
2017-07-07 10:04:05 +0530

    added changes

---
+++ src/app/midsection/midsection.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MidsectionComponent } from './midsection.component';
+
+describe('MidsectionComponent', () => {
+  let component: MidsectionComponent;
+  let fixture: ComponentFixture<MidsectionComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MidsectionComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MidsectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/midsection/midsection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-midsection',
+  templateUrl: './midsection.component.html',
+  styleUrls: ['./midsection.component.css']
+})
+export class MidsectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
