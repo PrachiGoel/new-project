8ca4b786434916ca5a8678fc9e54e91a2f3e5bd7
prachi <prachi.goel@comparepolicy.com>
2017-07-07 18:00:15 +0530

    added changes

--- src/app/midsection-routing.module.ts
+++ src/app/midsection-routing.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule , CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MidsectionComponent } from './midsection.component';
+import { MidsectionComponent } from './midsection/midsection.component';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './midsection/home/home.component';
+import { AboutComponent } from './midsection/about/about.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
